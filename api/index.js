402febdd040cf3b10121887b7f4d63a9101eaac9
Andrii Huts <andriihuts05@gmail.com>
2025-05-01 01:48:00 +0300

    added main route

--- api/index.js
+++ api/index.js
@@ -4,6 +4,7 @@ const cors = require('cors');
 const app = express();
 const PORT = 5000;
 
+const mainRoutes = require('./routes/main');
 const authRoutes = require('./routes/auth');
 const profileRoutes = require('./routes/profile');
 const feedRoutes = require('./routes/feed');
@@ -11,6 +12,7 @@ const feedRoutes = require('./routes/feed');
 app.use(cors());
 app.use(express.json());
 
+app.use('/', mainRoutes);
 app.use('/api/auth', authRoutes);
 app.use('/api/profile', profileRoutes);
 app.use('/api/feed', feedRoutes);
