f663f721352ed30636a359c8991a611458607a54
Andrii Huts <andriihuts05@gmail.com>
2025-05-01 05:10:03 +0300

    updated some codes

--- api/controllers/mainController.js
+++
@@ -1,3 +0,0 @@
-exports.main = async (req, res) => {
-    res.json({ 'message': 'OK' });
-};
--- api/index.js
+++ api/index.js
@@ -5,7 +5,6 @@ const cors = require('cors');
 const app = express();
 const PORT = 5000;
 
-const mainRoutes = require('./routes/main');
 const authRoutes = require('./routes/auth');
 const profileRoutes = require('./routes/profile');
 const feedRoutes = require('./routes/feed');
@@ -13,7 +12,7 @@ const feedRoutes = require('./routes/feed');
 app.use(cors());
 app.use(express.json());
 
-app.use('/', mainRoutes);
+app.get("/api/test", (req, res) => res.json({ ok: true }));
 app.use('/api/auth', authRoutes);
 app.use('/api/profile', profileRoutes);
 app.use('/api/feed', feedRoutes);
